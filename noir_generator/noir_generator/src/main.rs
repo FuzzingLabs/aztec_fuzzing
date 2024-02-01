4f47db144b0bebcccf06f6285970b947b6cf592b
afredefon <antoine.fredefon@etu.u-bordeaux.fr>
2024-02-02 00:53:04 +0100

    make it easy to use by anyone

--- noir_generator/noir_generator/src/main.rs
+++ noir_generator/noir_generator/src/main.rs
@@ -6,80 +6,70 @@ mod statements;
 mod constants;
 
 use std::process::Command;
-use std::sync::{Arc, Mutex};
-use std::thread;
+use std::io::{self, Write};
+
 extern crate honggfuzz;
 
-fn fuzz_by_command_line(data: Option<&[u8]>) -> Option<String> {
-    random::initialize_rng(data);
-    
-    let mut code_generated = String::new();
-    for _ in 0..random::gen_range(0, constants::NB_MAX_FUNCTION) {
-        code_generated = format!("{}{}\n", code_generated, generate_function::generate_function(random::gen_name()));
-    }
-    code_generated = format!("{}{}", code_generated, generate_function::generate_function("main".to_string()));
-
-    let nr_file_path = "/home/afredefon/FuzzingLabs/aztec_fuzzing/noir_generator/noir_generator/testNoir/test/src/main.nr";
-    std::fs::write(nr_file_path, &code_generated).expect("Failed to write temp file");
-
-    let compilation_result = Command::new("/home/afredefon/.nargo/bin/nargo")
-        .args(&["compile", "--program-dir", "/home/afredefon/FuzzingLabs/aztec_fuzzing/noir_generator/noir_generator/testNoir/test", "--package", "test"])
-        .output();
-
-    match compilation_result {
-        Ok(output) => {
-            if output.status.success() {
-                None
-            } else {
-                Some(String::from_utf8_lossy(&output.stderr).to_string())
-            }
-        }
-        Err(e) => {
-            eprintln!("Error executing compilation command: {}", e);
-            None
-        }
-    }
-}
 
 fn ignored_error(err: &String) -> bool{
     err.contains("attempt to divide by zero")
+    || err.contains("Comparisons are invalid on Field types.")
 }
 
-
 fn main() {
-    let num_threads = 4;
 
-    // Utilisez Arc (Atomic Reference Counter) pour partager les données entre les threads
-    let mut loop_count = 1;
-    let mut crash_count = 1;
+    let noir_project_dir = std::env::current_dir().unwrap().join("noir_project");
+    let nr_main_path = noir_project_dir.join("src/main.nr");
 
-    let _ = std::fs::remove_dir_all("/home/afredefon/FuzzingLabs/aztec_fuzzing/noir_generator/noir_generator/crash_found/");
-    std::fs::create_dir("/home/afredefon/FuzzingLabs/aztec_fuzzing/noir_generator/noir_generator/crash_found/");
+    let crash_dir = std::env::current_dir().unwrap().join("crashes_found");
 
-    loop {
-        match fuzz_by_command_line(None) {
-            Some(err) => {
-                if !ignored_error(&err) {
-                    let src = "/home/afredefon/FuzzingLabs/aztec_fuzzing/noir_generator/noir_generator/testNoir/test/src/main.nr";
-                    let dest = format!("/home/afredefon/FuzzingLabs/aztec_fuzzing/noir_generator/noir_generator/crash_found/crash{}.nr", crash_count.to_string());
-                    let _ = std::fs::copy(src, dest);
-    
-                    println!("CRASH N°{} FOUND AFTER {} LOOP", crash_count, loop_count);
-                    println!("{}", err);
-                    crash_count += 1;
-                }
-            }
-            None => {}
-        }
-        loop_count += 1;
+    if crash_dir.exists() {
+        std::fs::remove_dir_all(&crash_dir).expect("Failed to delete old crashes dir");
     }
 
-    // honggfuzz::fuzz!(|data: &[u8]| {
-    //     if let Some(err) = fuzz_by_command_line(Some(data)) {
-    //         if !ignored_error(&err) {
-    //             println!("CMD Error: {}", err);
-    //         }
-    //     }
-    // });
+    std::fs::create_dir_all(&crash_dir).expect("Failed to create the crashes dir");
+
+
+    let mut loop_count = 0;
+    let mut crash_count = 0;
+
+    loop {
+        random::initialize_rng(None);
+        let mut code_generated = String::new();
+        for _ in 0..random::gen_range(0, constants::NB_MAX_FUNCTION) {
+            code_generated = format!("{}{}\n", code_generated, generate_function::generate_function(random::gen_name()));
+        }
+        code_generated = format!("{}{}", code_generated, generate_function::generate_function("main".to_string()));
+        
+        std::fs::write(&nr_main_path, &code_generated).expect("Failed to write main.nr");
+
+        let compilation_result = Command::new("nargo")
+            .args(&["compile", "--program-dir", noir_project_dir.to_str().unwrap_or_else(|| panic!("Impossible de convertir le chemin en chaîne UTF-8 valide"))])
+            .output();
+
+        match compilation_result {
+            Ok(output) => {
+                if !output.status.success() {
+                    let err = String::from_utf8_lossy(&output.stderr).to_string();
+                    if !ignored_error(&err) {
+                        crash_count += 1;
+
+                        let crash = format!("crash{}", crash_count);
+
+                        std::fs::create_dir_all(&crash_dir.join(&crash)).expect("Failed to create a crash dir");
+                        std::fs::copy(&nr_main_path, &crash_dir.join(&crash).join("code.nr")).expect("Failed to copy the main.nr");
+                        std::fs::write(&crash_dir.join(&crash).join("err"), &err).expect("Failed to write err");
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Error executing compilation command: {}", e);
+            }
+        }
+        loop_count += 1;
+        
+        print!("\rLoop Count: {} Crash Count: {}", loop_count, crash_count);
+        io::stdout().flush().unwrap();
+    }
 
 }

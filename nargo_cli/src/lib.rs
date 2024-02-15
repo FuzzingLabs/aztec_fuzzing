faa2511204868c5f344582255301e8954cde5f77
afredefon <antoine.fredefon@etu.u-bordeaux.fr>
2024-02-16 00:26:24 +0100

    nargo_cli without hardcoded path

--- nargo_cli/src/lib.rs
+++ nargo_cli/src/lib.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::PathBuf;
 
 use backend_interface::Backend;
 use noirc_driver::CompileOptions;
@@ -8,7 +8,7 @@ pub mod backends;
 
 use crate::cli::{compile_cmd::CompileCommand, NargoConfig};
 use crate::errors::CliError;
-pub fn fuzzinglabs_run() -> Result<(), CliError> {
+pub fn fuzzinglabs_run(noir_project_dir: &PathBuf) -> Result<(), CliError> {
     let backend = Backend::new("acvm-backend-barretenberg".to_string());
     let args = CompileCommand {
         package: None,
@@ -19,7 +19,7 @@ pub fn fuzzinglabs_run() -> Result<(), CliError> {
             show_brillig: false,
             print_acir: false,
             deny_warnings: false,
-            silence_warnings: false,
+            silence_warnings: true,
             only_acir: false,
             disable_macros: false,
             expression_width: None,
@@ -29,8 +29,7 @@ pub fn fuzzinglabs_run() -> Result<(), CliError> {
         },
     };
     let config = NargoConfig {
-        program_dir: Path::new("/home/afredefon/FuzzingLabs/aztec_fuzzing/noir_generator/noir_project")
-            .to_path_buf(),
+        program_dir: noir_project_dir.to_path_buf(),
     };
     return cli::compile_cmd::run(&backend, args, config);
 }
